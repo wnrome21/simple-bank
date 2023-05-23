89100994f4a872a7ad0057d7f17332187516c166
nater <n.rome26@gmail.com>
2023-05-23 15:37:24 -0700

    added random values for unit testing "

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,14 +3,25 @@ package db
 import (
 	"context"
 	"testing"
+	"github.com/stretchr/testify/require"
+
 )
 
 func TestCreateAccount(t *testing.T) {
-	arg := CreateAccountParams{
-		Owner:    "Saul",
-		Balance:  1000000,
-		Currency: "USD",
+	arg := CreateAccountParams {
+		Owner:    , // randomly generated?
+		Balance:  ,
+		Currency: ,
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 }
